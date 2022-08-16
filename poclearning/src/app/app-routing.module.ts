a8e6cd2366feb1439ae583de7c5fa111c6173802
mohanleo <mohanasundaram@optisolbusiness.com>
2022-08-16 21:04:29 +0530

    review updates

--- poclearning/src/app/app-routing.module.ts
+++ poclearning/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TimepickerComponent } from './timepicker/timepicker.component';
 const routes: Routes = [
-  { path: 'time', component:TimepickerComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'time' },
+  { path: 'time', component:TimepickerComponent }
+  
 ];
 
 @NgModule({
--- poclearning/src/app/timepicker/timepicker.component.ts
+++ poclearning/src/app/timepicker/timepicker.component.ts
@@ -6,8 +6,21 @@ import * as moment from 'moment';
   styleUrls: ['./timepicker.component.scss']
 })
 export class TimepickerComponent implements OnInit {
-  
-
+  lasts1:number = 60;
+  lasts2:number = 0;
+  numb1:number = 0;
+  numb2:number = 0;
+  sm:any;
+  toggle: boolean = false;
+  last1:number = 13;
+  last2:number = 0;
+  num1:number = 0;
+  num2:number = 0;
+  dm:any;
+  colon:any;
+  msg:any;
+  convertedTime: any;
+  convertedTime1: any;
   constructor() {
    
    }
@@ -16,133 +29,130 @@ export class TimepickerComponent implements OnInit {
     
   }
  
-  toggle: boolean = false;
-  last1 = 13;
-  last2 = 0;
-  num1 = 0;
-  num2 = 0;
-  dm;
-  colon;
+ //function for increment the hours
+
   hourup(){
    
   if (this.num1 === this.last1) {
-    console.log("disable");
+    
   } else {
-    //this.num1++ ;
-    //console.log("Value of num1 after increment ", this.num1 );
+
     this.dm=this.num1++;
     this.dm=this.padLeadingZeros(this.dm)
-     console.log("Value of num1 after increment ", this.padLeadingZeros(this.num1));
+    
   }
   if(this.dm.length == 0){
-    console.log(this.dm);
+   
     }
     else{
       this.colon=":";
     }
   }
+
+  //function for Decrement  the hours
+
   hourdown(){
   if (this.num1 === this.last2) {
-    console.log("disable");
+    
   } else {
-   // this.num1--;
-   // console.log("Value of num1 after decr ", this.num1);
     this.dm=this.num1--;
     this.dm=this.padLeadingZeros(this.dm)
-     console.log("Value of num1 after decr ", this.padLeadingZeros(this.num1));
+   
   }
   if(this.dm.length == 0){
-    console.log(this.dm);
+  
     }
     else{
       this.colon=":";
     }
   }
+
+  //function for Increment  the Minutes
+
+  minup(){
+   
+    if (this.numb1 === this.lasts1) {
+
+    } else {
+     this.sm=this.numb1++;
+     this.sm=this.padLeadingZeros(this.sm)
+      
+    }
+    if(this.sm.length == 0){
+      }
+      else{
+        this.colon=":";
+      }
+    }
+
+    //function for Decrement  the Minutes
+
+    mindown(){
+    if (this.numb1 === this.lasts2) {
+     
+    } else {
+      this.sm=this.numb1--;
+     this.sm=this.padLeadingZeros(this.sm)
+    }
+    if(this.sm.length == 0){
+      }
+      else{
+        this.colon=":";
+      }
+    }
+     //function for AM Select
+    onItemSelectoram() {
+      this.msg='AM';
+        return this.msg;
+        
+      }
+
+    //function for PM Select
+
+      onItemSelectorpm() {
+        this.msg='PM';
+          return this.msg;
+        }  
+
+   //function for Input Change Value for Hours
+
   onChangeEvent(event: any){
     if (event.target.value >= 13) {
       event.target.value = 0;
       this.dm=0;
-      console.log("disable");
     } 
-    console.log(event.target.value);
+    
     this.dm=('0' + this.dm).slice(-2)
+    this.num1=this.dm;
   }
+
+   //function for Input Change Value for Minutes
+
   onChangeEvent1(event: any){
     if (event.target.value >= 60) {
       event.target.value = 0;
       this.sm=0;
-      console.log("disable");
     } 
-    console.log(event.target.value);
     this.sm=('0' + this.sm).slice(-2)
+    this.numb1=this.sm;
   }
-  lasts1 = 60;
-  lasts2 = 0;
-  numb1 = 0;
-  numb2 = 0;
-  sm;
-  minup(){
-   
-  if (this.numb1 === this.lasts1) {
-    console.log("disable");
-  } else {
-   // this.numb1++;
-   this.sm=this.numb1++;
-   this.sm=this.padLeadingZeros(this.sm)
-   //this.numb1 = this.padLeadingZeros(this.numb1++);
-   // console.log("Value of num1 after increment ", this.numb1);
-    console.log("Value of num1 after increment ", this.padLeadingZeros(this.numb1));
-  }
-  if(this.sm.length == 0){
-    console.log(this.dm);
-    }
-    else{
-      this.colon=":";
-    }
-  }
-  mindown(){
-  if (this.numb1 === this.lasts2) {
-    console.log("disable");
-  } else {
-    //this.numb1--;
-    this.sm=this.numb1--;
-   this.sm=this.padLeadingZeros(this.sm)
-    console.log("Value of num1 after decr ", this.padLeadingZeros(this.numb1));
-  }
-  if(this.sm.length == 0){
-    console.log(this.dm);
-    }
-    else{
-      this.colon=":";
-    }
-  }
-  
-  
-  msg:any;
-  onItemSelectoram() {
-  this.msg='AM';
-    return this.msg;
-    
-  }
-  onItemSelectorpm() {
-    this.msg='PM';
-      return this.msg;
-    }
+ 
+
+ //function for Submit the Results
+
     somethingChanged(e:any){
      
-      let t=e.target.value;
+      const str =String (this.dm)+':'+String(this.sm)+ ' '+String(this.msg);
       
-      const str = String(t);
-      
-    console.log(this.colon);
-    var convertedTime = moment(str, 'hh:mm A').format('HH:mm')
-    console.log(convertedTime);
+   
+    this.convertedTime1 = moment(str, 'hh:mm A').format('HH:mm A')
+    this.convertedTime = str;
+
         }
-    condatetime(){
-    }
+    
     padLeadingZeros(num) {
   
-      var s = num+"";
+      let s = num+"";
       let size =2;
     
       while (s.length < size) s = "0" + s;
